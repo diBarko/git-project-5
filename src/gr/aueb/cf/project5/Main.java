469f88f2f153fd1a525a4b6beecff07302313e51
Denis Barko <denisbarko@hotmail.com>
2024-02-10 23:32:25 +0200

    Main Updated

--- src/gr/aueb/cf/project5/Main.java
+++ src/gr/aueb/cf/project5/Main.java
@@ -3,6 +3,6 @@ package gr.aueb.cf.project5;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello GIT!");
+        System.out.println("Hello Coding Factory!!!!!!!");
     }
 }
